ff27365fd101187af31190ed59ddd92eb8b91751
Moses-Ongwenyi <moses.ongwenyi@ibm.com>
2020-04-13 19:35:17 +0300

    #1 setup Post database model and configured dynamic loading of data from DB to frontend.

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'Moses Wilfred',
-        'title': 'Blog Post 1',
-        'content': 'First Blog Post',
-        'date_posted': 'April 12, 2020'
-    },
-    {
-        'author': 'Emmah Moraa',
-        'title': 'Blog Post 2',
-        'content': 'Second Blog Post',
-        'date_posted': 'April 13, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
