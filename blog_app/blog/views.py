958f4e5bafdceacad25f55e80261fa64569d7612
Moses-Ongwenyi <moses.ongwenyi@ibm.com>
2020-04-13 07:32:44 +0300

    #1 adding the templates and styling them styling them using bootstrap.

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Moses Wilfred',
+        'title': 'Blog Post 1',
+        'content': 'First Blog Post',
+        'date_posted': 'April 12, 2020'
+    },
+    {
+        'author': 'Emmah Moraa',
+        'title': 'Blog Post 2',
+        'content': 'Second Blog Post',
+        'date_posted': 'April 13, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'Project'})
