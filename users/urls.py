3cab8ae8ae17710304bf52edda020babe0636193
Moses-Ongwenyi <moses.ongwenyi@ibm.com>
2020-04-19 16:10:16 +0300

    #7 #8 user registration and user login functionalities using the default Django builtins.

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from users import views
 
 urlpatterns = [
     path('register', views.register, name='users-register'),
-    path('login', views.login, name='users-login')
+    path('login', auth_views.LoginView.as_view(
+        template_name='users/login.html'), name='users-login'),
+    path('logout', auth_views.LogoutView.as_view(
+        template_name='users/logout.html'), name='users-logout'),
+    path('profile', views.profile, name='users-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from users.forms import UserRegisterForm
 
@@ -12,12 +13,13 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
 
-            return redirect('blog-home')
+            return redirect('users-login')
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'users/login.html')
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
